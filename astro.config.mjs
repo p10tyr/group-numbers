4f31e2d1bafed159e9d422e59b3d6673b1833b3d
Piotr Kula <pkula@live.co.uk>
2024-04-23 23:21:35 +0100

    config fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://p10tyr.github.io', 
+  base: '/group-numbers', 
+  output: 'static'
+});
